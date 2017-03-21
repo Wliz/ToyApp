6228c6a79230552e972cb51ec12a0d59e995c885
wanglizheng@skio.cn <15839447915aB>
2017-03-21 17:48:59 +0800

    add email format regexp valid to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+	# validates :name, presence: true
+	# 忽略大小写的email邮件地址正则表达式
+	VALID_EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+	validates_presence_of :name, :email
+	validates :name, length: { maximum: 50 }
+	validates :email, length: { maximum: 255 }, format: VALID_EMAIL_REGEXP
 end

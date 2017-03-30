39fa854a8bdf5d56089fc7e3a9f597558b6b3192
wanglizheng@skio.cn <15839447915aB>
2017-03-30 11:04:55 +0800

    implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,10 @@ class User < ApplicationRecord
 
 	# 用户密码确认
 	has_secure_password
+
+	# return string hash
+	def User.digest(string)
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+		BCrypt::Password.create(string, cost: cost)
+	end
 end

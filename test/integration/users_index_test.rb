6921c672e70b980965c86741ae8be0bff371a8af
wlz <wanglizheng@skio.cn>
2017-04-20 14:14:57 +0800

    delete user

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -5,6 +5,33 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
   def setup
-    @user = users(:michael)
+    @admin = users(:michael)
+    @non_admin = users(:archer)
+  end
+
+  test "index as admin including pagination and delete links" do
+    log_in_as(@admin)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    # ruby 2.4.0弃用Fixnum和Bignum into integer
+    # TODO 回头去看，然后查找原因吧
+    # first_page_of_users = User.paginate(page: 1)
+    # first_page_of_users.each do |user|
+    #   assert_select 'a[href=?]', user_path(user), text: user.name
+    #   unless user == @admin
+    #     assert_select 'a[href=?]', user_path(user), text: 'delete'
+    #   end
+    # end
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+    end
+  end
+
+  test 'index as no-admin' do
+    log_in_as @non_admin
+    get users_path
+
+    assert_select 'a', text: 'delete', count: 0
   end
 end
